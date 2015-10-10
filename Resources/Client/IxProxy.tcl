666167cebb66a59c42f9f66906c88b4f2ed011eb
jundong <jxu@ixiacom.com>
2015-10-10 16:12:51 +0800

    Support multiple user to use proxy server

--- Resources/Client/IxProxy.tcl
+++ Resources/Client/IxProxy.tcl
@@ -2,22 +2,55 @@ package req Itcl
 namespace import itcl::*
 
 class IxiaTcl {
-	
 	public variable ip
 	public variable port
+    public variable portList
 	
-	constructor { _ip _port } {
+	constructor { _ip { _port 4555 } } {
 		set ip $_ip
 		set port $_port
+        set portList [list 4556 4557 4558 4559 4560 4561 4562 4563 4564 4565 4566 4567 4568 4569 4570 4571 4572 4573 4574]
 	}
 	
 	method exec { args } {
-		set channel [ socket $ip $port ]
-		puts $channel $args
-		flush $channel
-		set result [ gets $channel ]
-		close $channel
-		return $result
+        set channel [ socket $ip $port ]
+        puts $channel $args
+        flush $channel
+        set result [ gets $channel ]
+        close $channel
+        return $result
 	}
 	
+	method format { str } {
+		return [string map {"###" "\n"} $str]
+	}
+    
+    method available {} {
+        set is_ready false
+        # Use configured port to try to connect to the remote proxy server
+        if { [ catch {
+            if { ![ $this exec info exist ::testName ] } {
+                set is_ready true 
+            }
+        } err ] } {
+            # User configured port is not available,
+            # so we'll try to use the port pool to find an available proxy server
+            foreach p $portList {
+                if { $p == $port } {
+                    continue
+                } else {
+                    if { [ catch {
+                        if { ![ $this exec info exist ::testName ] } {
+                            set is_ready true
+                            break
+                        }
+                    } err ] } {
+                        continue
+                    }
+                }
+            }
+        }
+        
+        return $is_ready
+    }
 }
--- Resources/Client/case.tcl
+++ Resources/Client/case.tcl
@@ -1,36 +1,63 @@
 source IxProxy.tcl
 
-IxiaTcl ixia localhost 4555
+set testName "IxLoad-HTTP-137"
+set flag false
+if { [ catch {
+    # The proxy port range is from 4555 to 4574,  we'll automatically try to
+    # use them one by one when we connect to the proxy server
+    #IxiaTcl ixia localhost
+    IxiaTcl ixia "172.16.174.137" 4556
+    
+    if { ![ixia available] } {
+        error "Proxy server: [ixia cget -ip] is busy on \
+        port: [ixia cget -port], please wait a while and try again."
+    } else {
+        puts "Connect to proxy server: [ixia cget -ip] on port: [ixia cget -port]"
+    }
+    
+    # This "::testName" must be set, we have to use to identify the test in Proxy Server side.
+    # It should be the first command to log all follow commands.
+    ixia exec set ::testName $testName
+    set flag true
+    
+    # If we don't use the latest IxLoad version in Proxy Server side,
+    # we can load your expecting version here for testing
+    #ixia exec source [file join "C:/Program Files (x86)/Ixia/IxLoad/6.70-EA/TclScripts/bin" IxiaWish.tcl]
+    
+    ixia exec package req IxLoad
+    ixia exec ::IxLoad connect localhost
+    
+    set testController [ ixia exec ::IxLoad new ixTestController -outputDir 1]
+    
+    ixia exec $testController setResultDir ${testName}@[clock format [ clock seconds ] -format %Y%m%d%H%M%S]
+    
+    #
+    # Load the repository
+    #
+    set repositoryName "Z:/Ixia/Workspace/IxTclProxy/Resources/Configs/$testName.rxf"
+    set repository [ ixia exec ::IxLoad new ixRepository -name $repositoryName ]
+    
+    #
+    # Loop through the tests, running them
+    #
+    set numTests [ ixia exec $repository testList.indexCount ]
+    for {set testNo 0} {$testNo < $numTests} {incr testNo} {
+    	set name [ixia exec $repository testList($testNo).cget -name]
+    	set test [ixia exec $repository testList.getItem $name]
 
-ixia exec source [file join "C:/Program Files (x86)/Ixia/IxLoad/6.70-EA/TclScripts/bin" IxiaWish.tcl]
-ixia exec package req IxLoad
-ixia exec ::IxLoad connect localhost
-
-set testController [ ixia exec ::IxLoad new ixTestController -outputDir 1]
-
-ixia exec $testController setResultDir "RESULTS/reprun"
-
-#
-# Load the repository
-#
-set repositoryName "C:/Ixia/Workspace/IxTclProxy/Resources/Configs/IxLoad-HTTP-137.rxf"
-set repository [ ixia exec ::IxLoad new ixRepository -name $repositoryName ]
-
-
-#
-# Loop through the tests, running them
-#
-#set numTests [ ixia exec $repository testList.indexCount ]
-#for {set testNo 0} {$testNo < $numTests} {incr testNo} {
-#    
-#	set testName [ixia exec $repository testList($testNo).cget -name]
-#
-#	set test [ixia exec $repository testList.getItem $testName]
-#
-#	# Start the test
-#	ixia exec $testController run $test
-#
-#	ixia exec vwait ::ixTestControllerMonitor
-#	ixia exec puts \$::ixTestControllerMonitor
-#
-#}
+    	# Start the test
+    	ixia exec $testController run $test
+    	ixia exec vwait ::ixTestControllerMonitor
+    }
+    
+    ixia exec $testController stopRun
+    ixia exec unset ::testName
+    
+    set log [ixia exec GetLogByTestName $testName]
+    puts [ixia format $log]
+} err ] } {
+	puts "Run test case failed:$err"
+    if { $flag } {
+        ixia exec unset ::testName
+    }
+}
